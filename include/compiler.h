6364ba291ceb3d1078034917b9049758261331bb
Nicole Cranon <nicole.cranon@gmail.com>
2015-09-05 19:13:21 -0600

    adding functions to compiler.h

--- include/compiler.h
+++ include/compiler.h
@@ -15,151 +15,190 @@ namespace compiler {
   enum opToken {
 
     MinusOp = scanner::MinusOp,
-    PlusOp = scanner::PlusOp
+		PlusOp = scanner::PlusOp
 
-  };
+	};
 
-  // types of expressions
-  enum exprKind {
+	// types of expressions
+	enum exprKind {
 
-    idExpr,
-    tempExpr,
-    literalExpr
+		idExpr,
+		tempExpr,
+		literalExpr
 
-  };
+	};
 
-  // placeholder for an arithmetic operator
-  struct opRec;
+	// placeholder for an arithmetic operator
+	struct opRec;
 
-  // place holder for an expression
-  struct exprRec;
+	// place holder for an expression
+	struct exprRec;
 
-  /*
-     generate
+	/*
+		 generate
 
-     precondition -
-     four strings:
-     s1 - operation code
-     s2 - operand1
-     s3 - operand2
-     s4 - result field
+		 precondition -
+		 four strings:
+		 s1 - operation code
+		 s2 - operand1
+		 s3 - operand2
+		 s4 - result field
 
-     postcondition - 
-     produces a correctly formatted instruction string in the output file
-     compile::outFile
-   */
-  void generate (const std::string& s1, 
-      const std::string& s2, 
-      const std::string& s3,
-      const std::string& s4);
+		 postcondition - 
+		 produces a correctly formatted instruction string in the output file
+		 compile::outFile
+	 */
+	void generate (const std::string& s1, 
+			const std::string& s2, 
+			const std::string& s3,
+			const std::string& s4);
 
-  /*
-     extract 
+	/*
+		 extract 
 
-     precondition -
-     requires a valid expression record (exprRec) object
+		 precondition -
+		 requires a valid expression record (exprRec) object
 
-     postcondition -
-     if type of expression record is idExpr or tempExpr, returns the name of
-     the expression record. if type is literalExpr (integer literal), 
-     returns the integer value of the expression record
-   */
-  std::string extract (const exprRec& e);
+		 postcondition -
+		 if type of expression record is idExpr or tempExpr, returns the name of
+		 the expression record. if type is literalExpr (integer literal), 
+		 returns the integer value of the expression record
+	 */
+	std::string extract (const exprRec& e);
 
-  /*
-     extractOp 
+	/*
+		 extractOp 
 
-     precondition -
-     requires a valid operator record (opRec) object
+		 precondition -
+		 requires a valid operator record (opRec) object
 
-     postcondition -
-     if type of op record type is PlusOp, returns "ADD, returns "SUB" 
-     otherwise
-   */
-  std::string extractOp (const opRec& o);
+		 postcondition -
+		 if type of op record type is PlusOp, returns "ADD, returns "SUB" 
+		 otherwise
+	 */
+	std::string extractOp (const opRec& o);
 
-  /*
-     lookUp 
+	/*
+		 lookUp 
 
-     precondition - 
-     the string name if an identifier
+		 precondition - 
+		 the string name if an identifier
 
-     postcondition - 
-     returns true if the given identifier is already in the symbol table
-     returns false otherwise
-   */
-  bool lookUp (const std::string& idName);
+		 postcondition - 
+		 returns true if the given identifier is already in the symbol table
+		 returns false otherwise
+	 */
+	bool lookUp (const std::string& idName);
 
-  /*
-     enter
+	/*
+		 enter
 
-     precondition -
-     the string name of an identifier
+		 precondition -
+		 the string name of an identifier
 
-     postcondition -
-     returns nothing. enters the given identifier into the symbol table
-   */
-  void enter (const std::string& idName);
+		 postcondition -
+		 returns nothing. enters the given identifier into the symbol table
+	 */
+	void enter (const std::string& idName);
 
-  /*
-     checkId
+	/*
+		 checkId
 
-     precondition - 
-     the name of an identifier
+		 precondition - 
+		 the name of an identifier
 
-     postcondition -
-     returns nothing. checks for the given id in the symbol table, if it is 
-     not in the symbol table it will add it, does nothing otherwise
-   */
-  void checkId (const std::string& idName);
+		 postcondition -
+		 returns nothing. checks for the given id in the symbol table, if it is 
+		 not in the symbol table it will add it, does nothing otherwise
+	 */
+	void checkId (const std::string& idName);
 
-  /*
-    getTemp
+	/*
+		 getTemp
 
-    precondition -
-    variable maxTemp (tracks the number of temporary variables already 
-    allocated thus far) exists and has been intialized
+		 precondition -
+		 variable maxTemp (tracks the number of temporary variables already 
+		 allocated thus far) exists and has been intialized
 
-    postcondition - 
-    checks if the temp variable id already exists in the
-    symbol table, if it does not it is entered into the symbol table.
-    the temporary id is returned
-   */
-  std::string getTemp ();
+		 postcondition - 
+		 checks if the temp variable id already exists in the
+		 symbol table, if it does not it is entered into the symbol table.
+		 the temporary id is returned
+	 */
+	std::string getTemp ();
 
-  /*
-     semantic routines
-   */
+	/*
+		 semantic routines
+	 */
 
-  /*
-    assign
+	/*
+		 assign
 
-    precondition -
-    a target id, and a a source expression record
+		 precondition -
+		 a target id, and a a source expression record
 
-    postcondition -
-    generates the code to store the source information in the target
-   */
-   void assign (const std::string& target, const exprRec& e);
+		 postcondition -
+		 generates the code to store the source information in the target
+	 */
+	void assign (const std::string& target, const exprRec& e);
 
-  /*
-    readId
+	/*
+		 readId
 
-    precondition - 
-    an expression record
+		 precondition - 
+		 an expression record
 
-    postcondition -
-    generates the code to read an id of type integer into provided 
-    expression record
-   */
-   void readId (exprRec& inVar);
+		 postcondition -
+		 generates the code to read an expression into provided 
+		 expression record
+	 */
+	void readId (exprRec& inVar);
 
-  /*
-    writeExpr
+	/*
+		 writeExpr
 
-    precondition -
-    the n 
-   */
+		 precondition -
+		 an expression record
+
+		 postcondition -
+		 generates the code to write an expression into the provided
+		 expression record
+	 */
+	void writeExpr ();
+
+	/*
+		 genInfix
+
+		 precondition -
+		 e1 (expression record 1), op (operator), e2 (expression record 2)
+
+		 postcondition -
+		 generates the code for eTemp = e1 op e2 (i.e. A + B), returns eTemp
+	 */
+	exprRec genInfix (const exprRec& e1, const opRec& op, const exprRec& e2);
+
+	/*
+		 processId
+
+		 precondition -
+		 an expression record
+
+		 postcondition -
+		 declare an id, enter it into the semantic table, and build a 
+		 corresponding semantic record
+	 */
+	void processId (exprRec& e);
+
+	/*
+		 processLiteral
+
+		 precondition -
+		 an expression record
+
+		 postcondition -
+		 convert literal to a numeric representation and build a semantic record
+	 */
+	void processLiteral (exprRec& e);
 
 }
 
